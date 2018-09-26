069b074f7603e1044fe00b5ac13caf58de8edd30
Egor Topal <Egor.Topal@cetera.com>
2018-09-26 09:04:22 -0700

    modified for setter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,63 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 class Doc {
-    private B record = new B();
-    private List<C> CRecords;
+    private B bRecord = new B();
+    private List<C> cRecords;
 
     Doc(){
-        CRecords = new ArrayList<C>();
-        CRecords.add(new C());
-        CRecords.add(new C());
+        cRecords = new ArrayList<C>();
+        cRecords.add(new C());
+        cRecords.add(new C());
     }
 
-    public B getRecord(){
-        return record;
+    public B getBRecord(){
+        return bRecord;
     }
 
     public List<C> getCRecords() {
-        return CRecords;
+        return cRecords;
     }
 }
 
 class A {
+    protected String value = "A";
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
     public void call() {
-        System.out.println("A");
+        System.out.println(value);
     }
 }
 
 class B extends A {
-    public void call1() {
-        System.out.println("B");
+    B() {this.value = "B";}
+
+    public void call_1() {
+        System.out.println("B --- " + value);
     }
 }
 
 class C extends A {
     private static int counter = 0;
     private int myNumber;
+
     C() {
+        this.value = "C";
         myNumber = counter++;
     }
-    public void call2() {
-        System.out.println("C" + myNumber);
+
+    public void call_2() {
+        System.out.println(value + myNumber);
+    }
+
+    public void setMyNumber(int value) {
+        this.myNumber = value;
     }
 }
 
@@ -51,7 +67,7 @@ public class Main {
     private <T extends A> List<T> getDocObjects(Class<T> cls, Doc doc) {
         List<T> arr = new ArrayList<T>();
         if (cls == B.class) {
-            arr.add((T)doc.getRecord());
+            arr.add((T)doc.getBRecord());
         }
         else if (cls == C.class) {
             for (C obj: doc.getCRecords()) {
@@ -61,26 +77,39 @@ public class Main {
         return arr;
     }
 
-    private <T> Method findMethod(Class<T> cls, String methodName) {
+    private boolean checkTypesFit(Type[] methodTypes, Type[] expectedTypes) {
+        if (methodTypes.length != expectedTypes.length) {
+            return false;
+        }
+        for(int i=0; i < methodTypes.length; i++) {
+            if(methodTypes[i] != expectedTypes[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private <T> Method findMethod(Class<T> cls, String methodName, Type... paramTypes) {
         if (cls == null) {
             return null;
         }
         for (Method m: cls.getDeclaredMethods()){
-            if (m.getName().equals(methodName)) {
+            if (m.getName().equals(methodName) && checkTypesFit(m.getParameterTypes(), paramTypes)) {
                 return m;
             }
         }
-        return findMethod(cls.getSuperclass(), methodName);
+        return findMethod(cls.getSuperclass(), methodName, paramTypes);
     }
 
-    private <T extends A> void invokeCallMethod(Doc doc, Class<T> cls, String methodName) {
-        Method foundMethod = findMethod(cls, methodName);
+    private <T extends A> void invokeCallMethod(Doc doc, Class<T> cls, String methodName, String newValue) {
+        Method foundMethod = findMethod(cls, methodName, String.class);
         if (foundMethod == null) {
-            throw new RuntimeException("Ha-Ha. No such method in given class (and it's ancestors)");
+            throw new RuntimeException("Ha-Ha. No method with given params in given class (and it's ancestors)");
         }
+
         for (T obj: getDocObjects(cls, doc)){
             try {
-                foundMethod.invoke(obj);
+                foundMethod.invoke(obj, newValue);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             } catch (InvocationTargetException e) {
@@ -93,6 +122,10 @@ public class Main {
         Main main = new Main();
         Doc doc = new Doc();
 
-        main.invokeCallMethod(doc, B.class, "aaa");
+        doc.getBRecord().call_1();
+        main.invokeCallMethod(doc, B.class, "setValue", "newValue");
+        doc.getBRecord().call_1();
+        main.invokeCallMethod(doc, B.class, "setMyNumber", "newValue");
+
     }
 }
